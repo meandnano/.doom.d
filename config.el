7843d54011fd059d04c0f0bbea24f4da9b177d13
= <meandnano@gmail.com>
2021-02-22 23:13:23 +0100

    Set regular font to Input Mono Narrow

--- config.el
+++ config.el
@@ -27,6 +27,8 @@
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-one)
 
+(setq doom-font (font-spec :family "Input Mono Narrow" :size 16))
+
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
